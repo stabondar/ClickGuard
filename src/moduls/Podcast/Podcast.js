f55e36ee4040c8d5b6bd30be55119e2718647a10
Denis Sizemov <96919055+Denis-Sizemov@users.noreply.github.com>
2022-12-22 12:40:23 +0200

    block change

--- src/moduls/Podcast/Podcast.js
+++ src/moduls/Podcast/Podcast.js
@@ -4,6 +4,22 @@ export default class Podcast
 {
     constructor()
     {
+        let mm = gsap.matchMedia();
+        let isMobile = '(max-width: 767px)';
 
+            mm.add(isMobile, () => 
+            {
+                const side = () =>
+                {
+                    let item = $('.podcast-template__left');
+                    let block = $('.speakers');
+                        
+                    $(item).each(function() {           
+                        item.appendTo(block);
+                    })
+                }
+
+        window.addEventListener('load', () => side())    
+    })
     }
 }
--- src/moduls/Slider.js
+++ src/moduls/Slider.js
@@ -33,34 +33,9 @@ export default class Slider
       swiperCard();
       window.addEventListener("resize", swiperCard);
 
-
-        // let swiper = new Swiper(".swiper", {
-        //     slidesPerView: "auto",
-        //     keyboard: true,
-        //     direction: "horizontal",
-        //     spaceBetween: 24,
-        //     speed: 800,
-        //     touchEventsTarget: "container",
-        //     grabCursor: true,
-        //     breakpoints: {
-        //       320: {
-        //         centeredSlides: true,
-        //         spaceBetween: 40
-        //       },
-        //       480: {
-        //         centeredSlides: true,
-        //         spaceBetween: 60
-        //       },
-        //       991: {
-        //         centeredSlides: false,
-        //         spaceBetween: null
-        //       }
-        //     }
-        // })
-
-        $('.slider__arrow').eq(1).on('click', () =>
+      $('.slider__arrow').eq(1).on('click', () =>
         {
-            swiper.slideNext()
+          swiper.slideNext()
         })
 
         $('.slider__arrow').eq(0).on('click', () =>
