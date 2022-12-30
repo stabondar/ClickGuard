45496c514754e11e0bf2471c67b642143cab59e4
Stas <96624704+stabondar@users.noreply.github.com>
2022-12-30 15:56:41 +0200

    Currency switcher

--- src/moduls/Pricing/js/Toggle.js
+++ src/moduls/Pricing/js/Toggle.js
@@ -10,62 +10,94 @@ export default class PricingToggle
     {
         const init = () =>
         {
+            // Currencies
+            let monthlyArray = 
+                [
+                    ['$89', '$149', '$199'],    //usd
+                    ['£89', '£149', '£199'],    //gbr
+                    ['€89', '€149', '€199'],    //euro
+                    ['C$89', 'C$149', 'C$199'], //cad
+                    ['$89', '$149', '$199']     //aud
+                ],
+                annualArray = 
+                [
+                    ['$74', '$119', '$159'],    //usd
+                    ['£74', '£119', '£159'],    //gbr
+                    ['€74', '€119', '€159'],    //euro
+                    ['C$74', 'C$119', 'C$159'], //cad
+                    ['$74', '$119', '$159']     //aud
+                ],
+                monthlyVal = [],
+                annualVal = []
+
+            annualVal.push(...annualArray[0])
+            monthlyVal.push(...monthlyArray[0])
+
             let list = $('.pricing__list'),
                 price0 = list.find('.h--56').eq(0),
                 price1 = list.find('.h--56').eq(1),
                 price2 = list.find('.h--56').eq(2),
                 toggle = $('.pricing__toggle--btn'),
                 circle = toggle.find('.pricing__toggle--circle'),
-                annual = $('.pricing__toggle').find('p').eq(0),
-                monthly = $('.pricing__toggle').find('p').eq(1),
+                annualText = $('.pricing__toggle').find('p').eq(0),
+                monthlyText = $('.pricing__toggle').find('p').eq(1),
                 chars = '$1234567890',
-                tl = gsap.timeline(
-                {
-                    paused: true
-                })
+                tl = gsap.timeline( { paused: true, defaults: { ease: 'none' } }),
+                mounthlyTL = gsap.timeline( { paused: true, defaults: { ease: 'none', duration: 1 } }),
+                annualTL = gsap.timeline( { paused: true, defaults: { ease: 'none', duration: 1 } })
 
-            gsap.set(annual, {color: '#0037ff', fontWeight: 700})
+            gsap.set(annualText, {color: '#0037ff', fontWeight: 700})
 
-            tl.to(price0, {scrambleText: {text: '$89', chars: chars}, duration: 1 })
-            .to(price1, {scrambleText: {text: '$149', chars: chars}, duration: 1 }, '<')
-            .to(price2, {scrambleText: {text: '$199', chars: chars}, duration: 1 }, '<')
-            .to(circle, {xPercent: 98}, '<')
-            .to(monthly, {color: '#0037ff', fontWeight: 700}, '<')
-            .to(annual, {color: '#080736', fontWeight: 500}, '<')
+            tl.to(circle, {xPercent: 98})
+            .to(monthlyText, {color: '#0037ff', fontWeight: 700}, '<')
+            .to(annualText, {color: '#080736', fontWeight: 500}, '<')
+
+            const mountlyRun = () =>
+            {
+                mounthlyTL.clear()
+                mounthlyTL.to(price0, {scrambleText: {text: monthlyVal[0], chars: chars} })
+                .to(price1, {scrambleText: {text: monthlyVal[1], chars: chars} }, '<')
+                .to(price2, {scrambleText: {text: monthlyVal[2], chars: chars} }, '<')
+            }
+
+            const annualRun = () =>
+            {
+                annualTL.clear()
+                annualTL.to(price0, {scrambleText: {text: annualVal[0], chars: chars} })
+                .to(price1, {scrambleText: {text: annualVal[1], chars: chars} }, '<')
+                .to(price2, {scrambleText: {text: annualVal[2], chars: chars} }, '<')
+            }
 
             //  Double Click Function
-            $(function () 
+            $(toggle).on('click', () => 
             {
-                let burgerClicks = [function () 
-                {
-                    tl.restart()
-                },
-
-                function () 
+                if(!toggle.hasClass('switched')) 
                 { 
-                    setTimeout(function () 
-                    {
-                        counter = 0;
-                    }, 200); 
-                    tl.timeScale(1.5).reverse()
-                } ]
+                    tl.restart()
 
-                let counter = 0
-                $(toggle).on('click', function () {
-                    if (counter >= 2) return false
-                    burgerClicks[counter]()
-                    counter++
-                })
-            })  
+                    mountlyRun()
+
+                    mounthlyTL.restart()
+                } else 
+                { 
+                    tl.timeScale(1.5).reverse()
+
+                    annualRun()
+
+                    annualTL.restart()
+                }
+                toggle.toggleClass('switched')
+            })
 
             /**
              * Drop Down Fucntions
              */
             let dropdown = $('.pricing__dp'),
-                dropdownTop = $('.pricing__dp--top'),
+                dropdownTop = $('.pricing__dp--top').find('.p--14'),
                 dropdownList = $('.pricing__dp--list'),
                 dropdownItem = $('.pricing__dp--item'),
                 dropdownArrow = $('.pricing__dp--arrow'),
+                currencyText,
                 dropdownTl = gsap.timeline({ paused: true, defaults: { duration: 0.3 }, onComplete: () => dropdown.addClass('open') })
 
             dropdownTl.from(dropdownList, {display: 'none', duration: 0})
@@ -75,13 +107,41 @@ export default class PricingToggle
             $('body').on('click', (event) => 
             {
                 let target = $(event.target)
-                if(target.closest('.pricing__dp').length > 0 )
+                if(target.closest(dropdown).length > 0 )
                 {
                     dropdownTl.play()
                 } else
                 {
                     dropdownTl.reverse()
                 }
+            })
+
+            $(dropdownItem).each(function(index)
+            {
+                let self = $(this)
+                self.on('click', () => 
+                {
+                    currencyText = self.find('p').text()
+                    monthlyVal = []
+                    monthlyVal.push(...monthlyArray[index])   
+
+                    annualVal = []
+                    annualVal.push(...annualArray[index])   
+
+                    gsap.to(dropdownTop, {scrambleText: {text: currencyText}})
+
+                    if(toggle.hasClass('switched')) 
+                    {
+                        mountlyRun()
+    
+                        mounthlyTL.restart()
+                    } else 
+                    {
+                        annualRun()
+    
+                        annualTL.restart()
+                    }
+                })
             })
 
         }
